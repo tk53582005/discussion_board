b56c66c6d960c2ad93e7a223d02638b41a0b4c86
tk53582005 <tk53582005@yahoo.co.jp>
2025-11-26 20:45:19 +0900

    Add Devise user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get "home/index"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -7,8 +10,11 @@ Rails.application.routes.draw do
 
   # Render dynamic PWA files from app/views/pwa/*
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+  get "manifest"       => "rails/pwa#manifest",       as: :pwa_manifest
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # Devise routes
+  # devise_for :users  # 一時的にコメントアウト
+
+  # Root path
+  root "home#index"
 end
