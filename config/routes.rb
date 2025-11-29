e5341e2fd113340c96121fb058740b5e2a80cfa0
tk53582005 <tk53582005@yahoo.co.jp>
2025-11-29 11:12:44 +0900

    Fix rubocop offenses

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # Boards routes with nested tasks and comments
   resources :boards do
     resources :tasks do
-      resources :comments, only: [:create, :destroy]
+      resources :comments, only: [ :create, :destroy ]
     end
   end
 
